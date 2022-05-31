fc8b275ea4c02733cc02f1fdb22440fa77502ef4
Ruman-Islam <rumanislam48@gmail.com>
2022-05-31 22:07:35 +0600

    h

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ const run = async () => {
             const result = await usersInfoCollection.findOne(filter);
             if (!result) {
                 const result = await usersInfoCollection.insertOne(userInfo);
-                return;
             }
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
             res.send({ accessToken: token });
